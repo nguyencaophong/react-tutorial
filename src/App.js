7347cdde36b95721bfd7d014d7f0078cf2c123fb
lifegoeson <phongnguyenw@gmail.com>
2022-09-25 21:33:32 +0700

    Preview avatar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import CleanUp from './components/Cleanup';
 import Content from './components/Content';
+import PreviewAvater from './components/PreviewAvatar';
 import Timer from './components/Timer';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
   return (
     <div style={{padding:20}}>
       <button onClick={()=> setShow(!show)}>TOGGER</button>
-      {show && <CleanUp />}
+      {show && <PreviewAvater />}
     </div>
   )
 }
---
+++ src/components/PreviewAvatar.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+
+function PreviewAvater(){
+
+    const [avatar,setAvatar] = useState();
+
+    useEffect(() =>{
+        // ** Cleanup
+        return ()=>{
+           avatar && URL.revokeObjectURL(avatar.preview)
+        }
+    },[avatar])
+
+
+    const handlePreviewAvatar = (e) =>{
+        const file = e.target.files[0];
+        file.preview = URL.createObjectURL(file);
+
+        setAvatar(file)
+    }
+
+    return(
+        <div>
+            <input type="file"
+            onChange={handlePreviewAvatar}
+            ></input>
+            {avatar && (
+                <img src={avatar.preview} alt="" width="80%" />
+            )}
+        </div>
+    )
+}
+
+
+export default PreviewAvater
