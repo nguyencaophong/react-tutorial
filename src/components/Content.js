4b5b623a9edcdfaacf0da5561c49989de312dfdf
lifegoeson <phongnguyenw@gmail.com>
2022-09-25 19:42:58 +0700

    useEffect(des)

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,37 +2,56 @@ import { useEffect, useState } from "react";
 
 
 
-// const tabs = ['posts','comments','albums'];
+const tabs = ['posts','comments','albums'];
 function Content(){
     const[title,setTitle] = useState('');
     const[posts,setPosts] = useState([]);
-    // const [type,setType] = useState('posts');
-
+    const [type,setType] = useState('posts');
     useEffect(() =>{
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        console.log('Type change',type);
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
         .then(value => {return value.json()})
         .then(data =>{
             setPosts(data);
         })
-    },[])
+    },[type]);
+
+    // useEffect(() => {
+    //     const hadleScroll = () => {
+    //         if(window.scrollY>=200){
+    //             setGoToTop(true);
+    //         }
+    //         else{
+    //             setGoToTop(false);
+    //         }
+    //     }
+
+    //     window.addEventListener('scroll',hadleScroll)
+    // },[])
 
     return (
         <div>
-            {/* {tabs.map(tab =>(
-                <button key={tab} onClick={() => setType(tab)}>{tab}</button>
-            ))} */}
-
+            {tabs.map(tab => (
+                <button key={tab}
+                    style={type===tab ?{
+                        color: '#fff',
+                        backgroundColor:'#333'
+                    }:{}}
+                    onClick={() => setType(tab)}
+                >
+                    {tab}
+                </button>
+            ))}
             <input 
             value={title}
             onChange={e => setTitle(e.target.value)}
             />
-
             <ul>
+                
                 {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
+                    <li key={post.id}>{post.title || post.name}</li>
                 ))}
             </ul>
-
         </div>
     )
 }
