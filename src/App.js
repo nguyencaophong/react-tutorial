598411d313989a4a56fdc48c6490f694bd796ffc
lifegoeson <phongnguyenw@gmail.com>
2022-09-25 22:00:19 +0700

    useRef

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import {useState} from 'react'
-import CleanUp from './components/Cleanup';
-import Content from './components/Content';
-import PreviewAvater from './components/PreviewAvatar';
-import Timer from './components/Timer';
+import {useEffect, useRef,useState} from 'react'
 
 function App() {
-  const [show, setShow] = useState(false);
+  const [count,setCount] = useState(60);
+  const timerId = useRef();
+  const preCount = useRef();
+
+  useEffect(() =>{
+    preCount.current = count;
+  },[count])
+
+  const handleStart = () =>{
+    timerId.current = setInterval(()=>{
+      setCount(pre=> pre - 1)
+    },1000)
+    console.log('Start => ',timerId.current);
+  }
+
+  const handelStop = ()=>{
+    clearInterval(timerId.current);
+    console.log('Stop =>',timerId.current);
+  }
+
+  console.log(count, preCount.current);
+
   return (
     <div style={{padding:20}}>
-      <button onClick={()=> setShow(!show)}>TOGGER</button>
-      {show && <PreviewAvater />}
+      <h1>{count}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handelStop}>Stop</button>
     </div>
   )
 }
